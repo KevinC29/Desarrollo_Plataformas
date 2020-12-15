1916f8466d6e51b55d6b7f56afac315bd5daa85b
KevinC29 <73950697+KevinC29@users.noreply.github.com>
2020-12-14 19:33:57 -0500

    practica_jquery
    base de datos, metodos

---
+++ practicas_jquery/crud1_jquery/insertar.php
@@ -0,0 +1,14 @@
+<?php
+include("database.php");
+
+if (isset($_POST["name"])) {
+    $name = $_POST["name"];
+    $description = $_POST["description"];    
+    $query = "INSERT INTO task(name, description) VALUES ('$name', '$description')";
+    $result = mysqli_query($conexion, $query);
+    if (!$result) {
+        die("La consulta de Insertar ha fallado");
+    }
+    echo "Se registro correctamente";
+    }
+?>
