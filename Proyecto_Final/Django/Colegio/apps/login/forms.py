0034f1b6d36d22926d71acca6443c16da6381d97
KevinC29 <73950697+KevinC29@users.noreply.github.com>
2021-02-27 20:49:26 -0500

    proyecto
    proyecto_final

--- Proyecto_Final/Django/Colegio/apps/login/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class FormularioLogin(forms.Form):
-    username= forms.CharField()
-    password= forms.CharField(widget=forms.PasswordInput())
--- Proyecto_Final/Django/Colegio/apps/matriculas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MatriculasConfig(AppConfig):
-    name = 'matriculas'
